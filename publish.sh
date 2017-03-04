d834c59df168257856f8754007537e3b0e2f309d
Alican Çubukçuoğlu <alicancubukcuoglu@gmail.com>
2017-03-05 02:25:32 +0300

    Use own boron until node:boron is fixed

--- publish.sh
+++ publish.sh
@@ -1,4 +1,11 @@
+# boron
+docker build -t alicanc/boron --pull ./boron/
+docker push alicanc/boron
+
+# boron-flow
 docker build -t alicanc/boron-flow --pull ./boron-flow/
 docker push alicanc/boron-flow
+
+# boron-flow-whatever-whatever-whatever
 docker build -t alicanc/boron-flow-imagemagick-ghostscript-ffmpeg --pull ./boron-flow-imagemagick-ghostscript-ffmpeg/
 docker push alicanc/boron-flow-imagemagick-ghostscript-ffmpeg
